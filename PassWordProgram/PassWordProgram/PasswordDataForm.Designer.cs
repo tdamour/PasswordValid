100460e951d9ced4a94391b18d97f21d1118eb7a
Tim DaMour <tim.damour@outlook.com>
2016-08-31 21:49:58 -0400

    upload files to repository

---
+++ PassWordProgram/PassWordProgram/PasswordDataForm.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace PassWordProgram
+{
+    partial class PasswordDataForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkPasswordButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.txtPasswordTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtFullNameTextBox = new System.Windows.Forms.TextBox();
+            this.txtUserNameTextBox = new System.Windows.Forms.TextBox();
+            this.lblUserName = new System.Windows.Forms.Label();
+            this.lblFullName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(68, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(916, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Please Enter your UserName, Full Name, and Password : ";
+            // 
+            // checkPasswordButton
+            // 
+            this.checkPasswordButton.Location = new System.Drawing.Point(341, 701);
+            this.checkPasswordButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.checkPasswordButton.Name = "checkPasswordButton";
+            this.checkPasswordButton.Size = new System.Drawing.Size(280, 117);
+            this.checkPasswordButton.TabIndex = 1;
+            this.checkPasswordButton.Text = "Check Password";
+            this.checkPasswordButton.UseVisualStyleBackColor = true;
+            this.checkPasswordButton.Click += new System.EventHandler(this.checkPasswordButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(731, 701);
+            this.exitButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(280, 117);
+            this.exitButton.TabIndex = 2;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // txtPasswordTextBox
+            // 
+            this.txtPasswordTextBox.Location = new System.Drawing.Point(311, 547);
+            this.txtPasswordTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtPasswordTextBox.Name = "txtPasswordTextBox";
+            this.txtPasswordTextBox.Size = new System.Drawing.Size(735, 38);
+            this.txtPasswordTextBox.TabIndex = 3;
+            this.txtPasswordTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(305, 484);
+            this.label2.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(262, 32);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Enter A Password : ";
+            // 
+            // txtFullNameTextBox
+            // 
+            this.txtFullNameTextBox.Location = new System.Drawing.Point(311, 371);
+            this.txtFullNameTextBox.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.txtFullNameTextBox.Name = "txtFullNameTextBox";
+            this.txtFullNameTextBox.Size = new System.Drawing.Size(735, 38);
+            this.txtFullNameTextBox.TabIndex = 4;
+            // 
+            // txtUserNameTextBox
+            // 
+            this.txtUserNameTextBox.Location = new System.Drawing.Point(311, 221);
+            this.txtUserNameTextBox.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.txtUserNameTextBox.Name = "txtUserNameTextBox";
+            this.txtUserNameTextBox.Size = new System.Drawing.Size(735, 38);
+            this.txtUserNameTextBox.TabIndex = 6;
+            // 
+            // lblUserName
+            // 
+            this.lblUserName.AutoSize = true;
+            this.lblUserName.Location = new System.Drawing.Point(303, 149);
+            this.lblUserName.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.lblUserName.Name = "lblUserName";
+            this.lblUserName.Size = new System.Drawing.Size(272, 32);
+            this.lblUserName.TabIndex = 7;
+            this.lblUserName.Text = "Enter A UserName : ";
+            // 
+            // lblFullName
+            // 
+            this.lblFullName.AutoSize = true;
+            this.lblFullName.Location = new System.Drawing.Point(303, 315);
+            this.lblFullName.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.lblFullName.Name = "lblFullName";
+            this.lblFullName.Size = new System.Drawing.Size(308, 32);
+            this.lblFullName.TabIndex = 8;
+            this.lblFullName.Text = "Enter Your Full Name : ";
+            // 
+            // PasswordDataForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1413, 863);
+            this.Controls.Add(this.lblFullName);
+            this.Controls.Add(this.lblUserName);
+            this.Controls.Add(this.txtUserNameTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtFullNameTextBox);
+            this.Controls.Add(this.txtPasswordTextBox);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.checkPasswordButton);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "PasswordDataForm";
+            this.Text = "Password Data";
+            this.Load += new System.EventHandler(this.PasswordDataForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button checkPasswordButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.TextBox txtPasswordTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtFullNameTextBox;
+        private System.Windows.Forms.TextBox txtUserNameTextBox;
+        private System.Windows.Forms.Label lblUserName;
+        private System.Windows.Forms.Label lblFullName;
+    }
+}
+
---
+++ PassWordProgram/PassWordProgram/PasswordDataForm.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PassWordProgram
+{
+    public partial class PasswordDataForm : Form
+    {
+        public PasswordDataForm()
+        {
+            InitializeComponent();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            //close the form
+            this.Close(); 
+        }
+
+        //The NumberUpperCase method accepts a stirng argument and returns the number of uppercase
+        //letters it contains. 
+
+        private int NumberUpperCase(string str)
+        {
+            int upperCase = 0;  //The number of uppercase letters... 
+
+            //Count the uppercase characters in str. 
+            foreach(char ch in str)
+            {
+                if (char.IsUpper(ch))
+                {
+                    upperCase++; 
+                }  
+            }
+            //return the number of uppercase characters.
+            return upperCase;
+        }
+
+        //isSpecialChar method accepts a string argument & return the special character it contains. 
+        private int isSpecialChar(string str)
+        {
+            int isSpecialChar=0; //char special 
+            
+            //find the special character in str
+            foreach(char ch in str)
+            {
+                if(!char.IsLetterOrDigit(ch))
+                {
+                    isSpecialChar++; 
+                }
+            }
+            //returns special character..
+            return isSpecialChar; 
+        }
+
+        //The NumberDigits method accepts a string argument and returns the number of digits
+        // it contains. 
+        private int NumberDigits(string str)
+        {
+            int digits = 0; //the number of digits 
+
+            //Count the digits in string
+            foreach(char ch in str)
+            {
+                if (char.IsDigit(ch))
+                {
+                    digits++; 
+                }
+                
+            }
+
+            //Return the number of digits.
+            return digits; 
+        }
+
+
+        //The denySpaces method denys spaces that entered as part of the password 
+
+        private int denySpaces(string str)
+        {
+            int spaces = 0;
+
+            foreach (char ch in str)
+            {
+                if (char.IsWhiteSpace(ch))
+                {
+                    spaces++;
+                } 
+            }
+
+            return spaces;
+        }
+
+        private void checkPasswordButton_Click(object sender, EventArgs e)
+        {
+            const int MIN_LENGTH = 8; //Password's minium length 
+
+            //get password from the TextBox 
+
+            string password = txtPasswordTextBox.Text;
+
+            //Validate the password 
+            // create var with fullName then equal to split txtFullNameTextBox.Text by space
+            var fullName = txtFullNameTextBox.Text.Split(null);
+            //then put grab the first memory array spot of full name into  the string first name 
+            string firstName = fullName[0];
+            //do the same for last name 
+            string lastName = fullName[1]; 
+
+            //Password Validation Logic 
+
+            if(password.Length >= MIN_LENGTH && NumberUpperCase(password) >=1 && isSpecialChar(password) >= 1 && NumberDigits(password) >= 1 && denySpaces(password)==0 && (!password.Contains(txtUserNameTextBox.Text)) && (!password.Contains(firstName)) && (!password.Contains(lastName)))
+            {
+                MessageBox.Show("The password is valid");
+            }
+            else
+            {
+                MessageBox.Show("The password does not meet " + "the requirements"); 
+            }
+        }
+
+        private void PasswordDataForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
